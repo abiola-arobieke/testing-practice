20ba6ffd9eaf8abcde0724dff62436dfd852331b
abiola <abiola.arobieke@gmail.com>
2023-04-14 11:35:12 -0400

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -1,30 +1,29 @@
 class Calculator {
-    constructor(firstNum, secondNum){
-        this.num1 = firstNum;
-        this.num2 = secondNum; 
-        this.result;    
-    };
+  constructor(firstNum, secondNum) {
+    this.num1 = firstNum;
+    this.num2 = secondNum;
+    this.result;
+  }
 
-    add(){
-        this.result = this.num1 + this.num2;
-        return this.result;
-    };
+  add() {
+    this.result = this.num1 + this.num2;
+    return this.result;
+  }
 
-    subtract(){
-        this.result = this.num1 - this.num2
-        return this.result
-    };
+  subtract() {
+    this.result = this.num1 - this.num2;
+    return this.result;
+  }
 
-    multiply(){
-        this.result = this.num1 * this.num2
-        return this.result
-    };
+  multiply() {
+    this.result = this.num1 * this.num2;
+    return this.result;
+  }
 
-    divide(){
-        this.result = this.num1 / this.num2
-        return this.result
-    }
-
+  divide() {
+    this.result = this.num1 / this.num2;
+    return this.result;
+  }
 }
 
-module.exports =  Calculator;
+module.exports = Calculator;
