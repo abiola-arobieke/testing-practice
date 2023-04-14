30c861a9c8ea9b892afe25191bd6998123f0029e
abiola <abiola.arobieke@gmail.com>
2023-04-14 08:18:12 -0400

    Add method to rverse a string

--- string.js
+++ string.js
@@ -7,10 +7,20 @@ const stringLength = (str) => {
       return 'Invalid character! Too long to too short!';
     }
   } else {
-    return 'Value is not a  string'
+    return 'Value is not a  string';
   }
 };
 
-module.exports = stringLength;
+const reverseString = (str) => {
+  if (typeof str === 'string') {
+    let newString = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+      newString += str[i];
+    }
+    return newString;
+  } else {
+    return 'Value is not a string!';
+  }
+};
 
-
+module.exports = { stringLength, reverseString };
